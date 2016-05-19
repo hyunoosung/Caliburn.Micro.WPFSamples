be49796839856a5b98cb6dc6cc74866055e8ba9a
hyounoo <hyounoosung@gmail.com>
2016-05-19 21:41:19 +0900

    Caliburn.Micro.Hello Initial Commit

---
+++ Caliburn.Micro.Hello/Caliburn.Micro.Hello/AppBootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Caliburn.Micro.Hello
+{
+    public class AppBootstrapper : BootstrapperBase
+    {
+        public AppBootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
---
+++ Caliburn.Micro.Hello/Caliburn.Micro.Hello/ShellViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Caliburn.Micro.Hello
+{
+    public class ShellViewModel : PropertyChangedBase
+    {
+        string name;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                NotifyOfPropertyChange(() => Name);
+                NotifyOfPropertyChange(() => CanSayHello);
+            }
+        }
+
+        public bool CanSayHello
+        {
+            get { return !string.IsNullOrWhiteSpace(Name); }
+        }
+
+        public void SayHello()
+        {
+            MessageBox.Show(string.Format("Hello {0}!", Name)); //Don't do this in real life :)
+        }
+    }
+}
