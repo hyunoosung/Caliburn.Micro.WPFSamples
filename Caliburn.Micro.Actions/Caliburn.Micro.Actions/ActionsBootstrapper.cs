1f73abd2f8fc2f7596d31a4bb33023ce6da464f2
hyounoo <hyounoosung@gmail.com>
2016-05-19 21:41:50 +0900

    Caliburn.Micro.Actions Initial Commit

---
+++ Caliburn.Micro.Actions/Caliburn.Micro.Actions/ActionsBootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caliburn.Micro.Actions
+{
+    public class ActionsBootstrapper : BootstrapperBase
+    {
+        public ActionsBootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, System.Windows.StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
---
+++ Caliburn.Micro.Actions/Caliburn.Micro.Actions/ShellViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caliburn.Micro.Actions
+{
+    public class ShellViewModel : PropertyChangedBase
+    {
+        private string _message;
+
+        public string Message
+        {
+            get { return _message; }
+            set
+            {
+                _message = value;
+                NotifyOfPropertyChange(() => Message);
+                NotifyOfPropertyChange(() => CanChangeMessage);
+                NotifyOfPropertyChange(() => CanChangeMessage2);
+            }
+        }
+
+        
+
+        private int _pressCount;
+
+
+        public bool CanChangeMessage
+        {
+            get { return _pressCount < 10; }
+        }
+
+        public void ChangeMessage()
+        {
+            _pressCount++;
+
+            Message = "Presses = " + _pressCount;
+        }
+
+        public bool CanChangeMessage2
+        {
+            get { return _pressCount < 20; }
+        }
+
+        public void ChangeMessage2(int incrementBy)
+        {
+            _pressCount += incrementBy;
+
+            Message = "Presses = " + _pressCount;
+        }
+
+        public ShellViewModel()
+        {
+            Message = "Hello World";
+        }
+    }
+}
