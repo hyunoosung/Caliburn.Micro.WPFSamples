7dcd2453aacf0c9b2f7b2a284af18ec840cd2f9b
hyounoo <hyounoosung@gmail.com>
2016-05-21 21:57:39 +0900

    EventAggregator implemented

---
+++ Caliburn.Micro.EventAggregators/Caliburn.Micro.EventAggregators/ShellViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caliburn.Micro.EventAggregators
+{
+    public class ShellViewModel : PropertyChangedBase
+    {
+        public ButtonsViewModel ButtonsVM
+        {
+            get;
+            set;
+        }
+
+        public TextViewModel TextVM
+        {
+            get;
+            set;
+        }
+
+        public ShellViewModel()
+        {
+            IEventAggregator events = new EventAggregator();
+
+            ButtonsVM = new ButtonsViewModel(events);
+            TextVM = new TextViewModel(events);
+        }
+    }
+}
---
+++ Caliburn.Micro.EventAggregators/Caliburn.Micro.EventAggregators/TextViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caliburn.Micro.EventAggregators
+{
+    public class TextViewModel : PropertyChangedBase, IHandle<ChangeTextMessage>
+    {
+
+        private string _text;
+
+        public string Text
+        {
+            get { return _text; }
+            set
+            {
+                _text = value;
+                NotifyOfPropertyChange(() => Text);
+            }
+        }
+
+        private IEventAggregator _events;
+
+        public TextViewModel(IEventAggregator events)
+        {
+            _events = events;
+
+            _events.Subscribe(this);
+
+            Text = "No buttons pressed";
+        }
+
+        public void Handle(ChangeTextMessage message)
+        {
+            Text = message.Text; 
+        }
+    }
+}
